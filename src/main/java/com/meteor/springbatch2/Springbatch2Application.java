3120782ce6c1c14f49ea8bd8854e6a008d6457a6
tjdans34568 <tjdans34568@gmail.com>
2022-01-11 16:46:06 +0900

    Initial commit

---
+++ src/main/java/com/meteor/springbatch2/Springbatch2Application.java
@@ -0,0 +1,15 @@
+package com.meteor.springbatch2;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing // 배치 기능 활성화
+@SpringBootApplication
+public class Springbatch2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbatch2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/meteor/springbatch2/Springbatch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteor.springbatch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbatch2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
